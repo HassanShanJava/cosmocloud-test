2fd5769b5e0206bd8fbb0b7eda0ed5a2c18df100
HassanShanJava <hassanshan675@gmail.com>
2023-04-18 01:27:43 +0500

    intialize test project

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import React from "react"
+
+function App() {
+  return (
+    <div className="max-w-4xl mx-auto bg-red-900">
+      <p className="text-2xl max-w-xl mx-auto">CosmoCloud test</p>
+    </div>
+  )
+}
+
+export default App
